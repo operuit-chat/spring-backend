c02e3946babea453d916a1619694711ea5e35362
ShortyDev <jakob123413@gmail.com>
2021-11-20 00:19:57 +0100

    added login logic + cleanup

--- src/main/java/at/operuit/restapi/routes/AuthController.java
+++ src/main/java/at/operuit/restapi/routes/AuthController.java
@@ -3,12 +3,10 @@ package at.operuit.restapi.routes;
 import at.operuit.restapi.OperuitMain;
 import at.operuit.restapi.database.query.QueryResult;
 import at.operuit.restapi.models.Response;
+import at.operuit.restapi.models.User;
 import at.operuit.restapi.util.data.Hashing;
 import at.operuit.restapi.util.data.RateLimiter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -16,12 +14,15 @@ import java.util.concurrent.CompletableFuture;
 @ResponseBody
 public class AuthController {
 
-    @GetMapping("/register")
-    public Response<String> register(@RequestHeader("User-Data") String requestData, String username, String displayName, String password) {
+    @PostMapping("/register")
+    public Response<String> register(@RequestHeader("User-Data") String requestData, @RequestBody User user) {
         if (!RateLimiter.compute(Hashing.hash(requestData)).acquire())
             return new Response<>(50, "Rate limit exceeded");
+        String username = user.username();
+        String displayName = user.displayName();
+        String password = user.password();
         if (username == null || displayName == null || password == null
-                || username.length() < 3 || displayName.length() < 3 || password.length() < 3
+                || displayName.length() < 3 || password.length() < 3
                 || username.length() != 64 || displayName.length() > 128 || password.length() > 4096)
             return new Response<>(100, "One or more arguments provided are missing or invalid");
         CompletableFuture<QueryResult> query = OperuitMain.getInstance().getDatabaseService().execute(() -> "SELECT * FROM `users` WHERE `username` = ?", username);
@@ -31,6 +32,36 @@ public class AuthController {
         OperuitMain.getInstance().getDatabaseService().execute(() -> "INSERT INTO `users` (`username`, `display_name`, `password`) VALUES (?, ?, ?)",
                 username, displayName, password).thenAccept((queryResult -> System.out.println("User " + username + " registered")));
         return new Response<>(200, "Success");
+    }
+
+    @PostMapping("/login")
+    public Response<String> login(@RequestHeader("User-Data") String requestData, @RequestBody User user) {
+        if (!RateLimiter.compute(Hashing.hash(requestData)).acquire())
+            return new Response<>(50, "Rate limit exceeded");
+        String username = user.username();
+        String password = user.password();
+        if (username == null || password == null
+                || password.length() < 3
+                || username.length() != 64 || password.length() > 4096)
+            return new Response<>(100, "One or more arguments provided are missing or invalid");
+        CompletableFuture<QueryResult> query = OperuitMain.getInstance().getDatabaseService().execute(() -> "SELECT * FROM `users` WHERE `username` = ? AND `password` = ?", username, password);
+        QueryResult result = query.join();
+        if (result.getRows().size() == 0)
+            return new Response<>(102, "Auth error");
+        return new Response<>(200, "Success");
+    }
+
+    @GetMapping("/salt")
+    public Response<String> retrieveSalt(@RequestHeader("User-Data") String requestData, @RequestParam String username) {
+        if (!RateLimiter.compute(Hashing.hash(requestData)).acquire())
+            return new Response<>(50, "Rate limit exceeded");
+        if (username == null || username.length() != 64)
+            return new Response<>(100, "One or more arguments provided are missing or invalid");
+        CompletableFuture<QueryResult> query = OperuitMain.getInstance().getDatabaseService().execute(() -> "SELECT * FROM `users` WHERE `username` = ?", username);
+        QueryResult result = query.join();
+        if (result.getRows().size() == 0)
+            return new Response<>(101, "User does not exist");
+        return new Response<>(200, result.getRows().get(0).get("password").toString().split(":")[1]);
     }
 
 }
