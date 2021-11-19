d14ff0215580eb264e31162577ce6c46088a68a4
ShortyDev <jakob123413@gmail.com>
2021-11-20 00:24:26 +0100

    removed useless TaskExecutor

--- src/main/java/at/operuit/restapi/OperuitBackendApplication.java
+++ src/main/java/at/operuit/restapi/OperuitBackendApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @EnableAsync
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class, R2dbcAutoConfiguration.class})
@@ -22,17 +18,6 @@ public class OperuitBackendApplication {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    @Bean
-    public Executor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("async-");
-        executor.initialize();
-        return executor;
     }
     
 }
