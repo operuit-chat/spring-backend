abf6f6c675cfcc4b332c6d0008ca0b5aa66abef1
ShortyDev <jakob123413@gmail.com>
2021-11-20 17:04:51 +0100

    made Response class be a record

--- src/main/java/at/operuit/restapi/models/Response.java
+++ src/main/java/at/operuit/restapi/models/Response.java
@@ -1,11 +1,4 @@
 package at.operuit.restapi.models;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class Response<T> {
-    
-    public int code;
-    public T message;
-
+public record Response<T>(int code, T message) {
 }
