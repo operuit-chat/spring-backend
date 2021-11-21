d7fd91eb3358e068f7251ed45324e2d469ef9e77
MongoTarzan <tizian.k@gmx.at>
2021-11-21 14:58:59 +0100

    prettify

--- src/main/java/at/operuit/restapi/util/data/Hashing.java
+++ src/main/java/at/operuit/restapi/util/data/Hashing.java
@@ -13,9 +13,8 @@ public class Hashing {
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
             byte[] hash = messageDigest.digest(message.getBytes(StandardCharsets.UTF_8));
             StringBuilder stringBuilder = new StringBuilder();
-            for (byte b : hash) {
+            for (byte b : hash)
                 stringBuilder.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
-            }
             return stringBuilder.toString();
         } catch (Exception e) {
             throw new RuntimeException(e);
